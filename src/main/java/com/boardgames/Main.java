76f454173394280c0d6055194c279c7fabc564c8
GlassCol <coleglass18@outlook.com>
2022-07-27 14:11:48 -0500

    Main Page Styling Completed
    The styling for the main page has been completed

--- src/main/java/com/boardgames/Main.java
+++ src/main/java/com/boardgames/Main.java
@@ -6,14 +6,16 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-menu-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 500, 350);
+        stage.setTitle("Board Games");
         stage.setScene(scene);
+        scene.getStylesheets().add(Objects.requireNonNull(Main.class.getResource("stylesheet.css")).toExternalForm());
         stage.show();
     }
 
