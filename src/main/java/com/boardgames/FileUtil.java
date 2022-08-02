719c2cd89f9b6feeaa2984936a2dd3d343d3f241
nuttdguy <pygnasakdev@gmail.com>
2022-08-01 20:53:46 -0500

    Add FileUtil class for reading and writing

--- src/main/java/com/boardgames/FileUtil.java
+++ src/main/java/com/boardgames/FileUtil.java
@@ -1,4 +1,43 @@
 package com.boardgames;
 
+import java.io.*;
+import java.nio.file.Path;
+
 public class FileUtil {
+
+
+    public static void writeToAFile(String data, String fileName) {
+        File file = new File(fileName);
+        boolean append = false;
+
+        if (file.exists()) { append = true; }
+
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(file.getAbsolutePath(), append);
+            fileOutputStream.write(data.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String readAFile(String fileName) {
+        StringBuilder data =  new StringBuilder();;
+
+        try {
+            FileInputStream fileInputStream = new FileInputStream(Path.of(fileName).toString());
+            int byteData = fileInputStream.read();
+            data.append( (char) byteData);
+
+            while (byteData != -1) {
+                byteData = fileInputStream.read();
+                data.append((char) byteData);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return data.toString();
+    }
+
 }
