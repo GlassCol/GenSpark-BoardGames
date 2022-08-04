dd2f56dbd907319954171a6851509884c32188cd
GlassCol <coleglass18@outlook.com>
2022-08-04 15:47:12 -0500

    Added the gameboard to the chess view + naigatoin to both chess and checkers views (checkers in not yet implemented)

--- src/main/java/com/boardgames/controller/ChessDisplayController.java
+++ src/main/java/com/boardgames/controller/ChessDisplayController.java
@@ -1,4 +1,59 @@
 package com.boardgames.controller;
 
-public class ChessDisplayController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.StrokeType;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ChessDisplayController implements Initializable {
+    @FXML
+    GridPane scoreBoardGridPane;
+    @FXML
+    GridPane boardGridPane;
+    Rectangle[][] board;
+
+    public void colorBoard() {
+        // Declares new board
+        board = new Rectangle[8][8];
+
+        // Initializes new board
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+                board[i][j] = new Rectangle();
+                board[i][j].setWidth(100);
+                board[i][j].setHeight(100);
+                board[i][j].setStroke(Color.TRANSPARENT);
+                board[i][j].setStrokeType(StrokeType.INSIDE);
+                board[i][j].setStrokeWidth(1);
+            }
+        }
+        // Generates colours for the chessboard backgrounds
+        for(int i=0; i < 7; i++){
+            for(int j=0; j < 7; j++){
+                if((i%2==0 && j%2==1) || (i%2==1 && j%2==0)){
+                    board[i][j].setFill(Color.WHITE);
+                }
+                else if((i%2==0 && j%2==0) || (i%2==1 && j%2==1)){
+                    board[i][j].setFill(Color.DARKGREEN);
+                }
+                boardGridPane.add(board[i][j], i, j);
+            }
+        }
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        boardGridPane.setPadding(new Insets(0,0,0,0));
+        colorBoard();
+    }
 }
+
--- src/main/java/com/boardgames/controller/MainMenuController.java
+++ src/main/java/com/boardgames/controller/MainMenuController.java
@@ -1,6 +1,7 @@
 package com.boardgames.controller;
 
 import com.boardgames.Main;
+import com.boardgames.Settings;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -17,6 +18,19 @@ public class MainMenuController {
     private Button exitButton;
     @FXML
     private Button settingsButton;
+    @FXML
+    protected void onStartGameButton() throws IOException {
+        Stage stage = (Stage) settingsButton.getScene().getWindow();
+        stage.close();
+        Stage mainStage = new Stage();
+        FXMLLoader fxmlLoader;
+        if(Settings.isPlayingChess())fxmlLoader = new FXMLLoader(Main.class.getResource("chess-view.fxml"));
+        else fxmlLoader = new FXMLLoader(Main.class.getResource("checkers-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 1000);
+        mainStage.setTitle("Board Games");
+        mainStage.setScene(scene);
+        mainStage.show();
+    }
     @FXML
     protected void onSettingsButtonClick() throws IOException {
         Stage stage = (Stage) settingsButton.getScene().getWindow();
