65e14e36227c3887b65873eed8cf02aa451e6da4
GlassCol <coleglass18@outlook.com>
2022-08-05 14:40:21 -0500

    Finished initializing the tileArray to its start game state

--- src/main/java/com/boardgames/controller/ChessDisplayController.java
+++ src/main/java/com/boardgames/controller/ChessDisplayController.java
@@ -1,5 +1,8 @@
 package com.boardgames.controller;
 
+import com.boardgames.Tile;
+import com.boardgames.piece.Piece;
+import com.boardgames.piece.chesspiece.*;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
@@ -21,7 +24,100 @@ public class ChessDisplayController implements Initializable {
     GridPane boardGridPane;
 
     Rectangle[][] board;
+    Tile[][] tileArray = new Tile[8][8];
 
+    /**
+     * Places all Pieces into their designated tile for the start of the game
+     * @return tileArray : Tile[][], The tile set in the starting game state
+     */
+    public Tile[][] setUpPieces(){
+        setUpPawns();
+        setUpRooks();
+        setUpBishops();
+        setUpKnights();
+        setUpQueens();
+        setUpKings();
+        return tileArray;
+    }
+    /**
+     * Places all Kings into their designated tile for the start of the game
+     */
+    public void setUpKings(){
+        Piece blackKing = new King();
+        blackKing.setColor("Black");
+        Piece whiteKing = new King();
+        blackKing.setColor("White");
+        tileArray[4][0].getPieceList().add(blackKing);
+        tileArray[4][7].getPieceList().add(whiteKing);
+    }
+    /**
+     * Places all Queens into their designated tile for the start of the game
+     */
+    public void setUpQueens(){
+        Piece blackQueen = new Queen();
+        blackQueen.setColor("Black");
+        Piece whiteQueen = new Queen();
+        blackQueen.setColor("White");
+        tileArray[3][0].getPieceList().add(blackQueen);
+        tileArray[3][7].getPieceList().add(whiteQueen);
+    }
+    /**
+     * Places all Bishops into their designated tile for the start of the game
+     */
+    public void setUpBishops(){
+        Piece blackBishop = new Bishop();
+        blackBishop.setColor("Black");
+        Piece whiteBishop = new Bishop();
+        blackBishop.setColor("White");
+        tileArray[2][0].getPieceList().add(blackBishop);
+        tileArray[5][0].getPieceList().add(blackBishop);
+        tileArray[2][7].getPieceList().add(whiteBishop);
+        tileArray[5][7].getPieceList().add(whiteBishop);
+    }
+    /**
+     * Places all Knights into their designated tile for the start of the game
+     */
+    public void setUpKnights(){
+        Piece blackKnight = new Knight();
+        blackKnight.setColor("Black");
+        Piece whiteKnight = new Knight();
+        blackKnight.setColor("White");
+        tileArray[1][0].getPieceList().add(blackKnight);
+        tileArray[6][0].getPieceList().add(blackKnight);
+        tileArray[1][7].getPieceList().add(whiteKnight);
+        tileArray[6][7].getPieceList().add(whiteKnight);
+    }
+    /**
+     * Places all Rooks into their designated tile for the start of the game
+     */
+    public void setUpRooks(){
+        Piece blackRook = new Rook();
+        blackRook.setColor("Black");
+        Piece whiteRook = new Rook();
+        blackRook.setColor("White");
+        tileArray[0][0].getPieceList().add(blackRook);
+        tileArray[7][0].getPieceList().add(blackRook);
+        tileArray[0][7].getPieceList().add(whiteRook);
+        tileArray[7][7].getPieceList().add(whiteRook);
+    }
+    /**
+     * Places all Pawns into their designated tile for the start of the game
+     */
+    public void setUpPawns(){
+        for (int i = 0; i < 8; i++) {
+            Piece pawn = new Pawn();
+            pawn.setColor("Black");
+            tileArray[1][i].getPieceList().add(pawn);
+        }
+        for (int i = 0; i < 8; i++) {
+            Piece pawn = new Pawn();
+            pawn.setColor("White");
+            tileArray[6][i].getPieceList().add(pawn);
+        }
+    }
+    /**
+     * Creates a backdrop fo the board which the game will be played on
+     */
     public void colorBoard() {
         // Declares new board
         board = new Rectangle[8][8];
@@ -68,9 +164,9 @@ public class ChessDisplayController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        boardGridPane.setPadding(new Insets(0,0,0,0));
         addScoreBoard(scoreBoardStackPane);
         colorBoard();
+        setUpPieces();
     }
 }
 
